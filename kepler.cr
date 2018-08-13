85412908a00de9507a593aec82fda0f808da33e0
Jun Makino <makino@localhost.localdomain>
2018-08-13 22:47:26 +0900

    new integrator added to kepler.cr

--- kepler.cr
+++ kepler.cr
@@ -18,6 +18,15 @@ def leapfrog_1step(x,v,dt)
   v = v+ dt*(a+newa)*0.5
   [x,v]
 end
+def yoshida4(x,v,dt)
+  
+  d1 = 1/(2-exp(log(2)/3))
+  d2 = 1-2*d1
+  x,v=leapfrog_1step(x,v,dt*d1)
+  x,v=leapfrog_1step(x,v,dt*d2)
+  x,v=leapfrog_1step(x,v,dt*d1)
+  [x,v]
+end
 
 def e(x,v)
   pot(x)+v*v*0.5
@@ -25,18 +34,25 @@ end
 v0=ARGV[0].to_f
 dt=ARGV[1].to_f
 tend=ARGV[2].to_f
+type=ARGV[3].to_i
 dtreal=dt*(Math::PI*2)
-STDERR.print "v0=#{v0}, dt=#{dt}, tend=#{tend}\n"
+STDERR.print "v0=#{v0}, dt=#{dt}, tend=#{tend} type={#type}\n"
 n = ((tend+0.5*dt)/dt).to_i
 v=[0.0,v0,0.0].to_v
 x=[1.0,0.0,0.0].to_v
 e0=e(x,v)
 ninter = n/100
 ninter = 1 if ninter == 0
+    printf("%25.20e %25.20e %25.20e %25.20e %25.20e %25.20e %d\n", 0,
+           x[0],x[1], v[0],v[1], 0,0)
 n.times{|i|
-  x,v = leapfrog_1step(x,v,dtreal)
-  if i%ninter == 0
-    printf("%25.20e %25.20e %25.20e %25.20e %25.20e %25.20e %d\n", i*dtreal,
+  if type==0	
+    x,v = leapfrog_1step(x,v,dtreal)
+  else
+    x,v = yoshida4(x,v,dtreal)
+  end
+  if (i+1)%ninter == 0
+    printf("%25.20e %25.20e %25.20e %25.20e %25.20e %25.20e %d\n", (i+1)*dtreal,
            x[0],x[1], v[0],v[1], e(x,v)-e0,i)
   end
 }
